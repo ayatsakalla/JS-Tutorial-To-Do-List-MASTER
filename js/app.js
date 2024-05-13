8aa09ef5e149ba391e755b1033531394cebdc3ce
ayatsakalla <ayat.sakalla@outlook.com>
2024-05-13 14:47:32 -0400

    learned/did insert adjacent html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // CODE EXPLAINED channel
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById("date");
+const input = document.getElementById("input");
+
+
+function addToDo(toDo) {
+    const list = document.getElementById("list");
+    const text = `<li class="item">
+                <i class="co fa fa-circle-thin" job="complete"></i>
+                <p class="text">${toDo}</p>
+                <i class="de fa fa-trash-o" job="delete"></i>
+            </li>`
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
+
+
