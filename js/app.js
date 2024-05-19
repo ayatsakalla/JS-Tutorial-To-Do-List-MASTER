c120df712d786bc3e0ada7066cc18aea340eec1e
ayatsakalla <144719331+ayatsakalla@users.noreply.github.com>
2024-05-19 19:03:05 +0000

    updated to do section

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 // CODE EXPLAINED channel
 const clear = document.querySelector(".clear");
-const dataElement = document.getElementById("date");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
 const input = document.getElementById("input");
 
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 
-function addToDo(toDo, ) {
-    const list = document.getElementById("list");
+
+function addToDo(toDo, id, done, trash) {
+
+    if (trash) {return; }
+    
+    const DONE = done ? CHECK : UNCHECK; 
+    const LINE = done ? LINE_THROUGH : "";
+    
     const text = `<li class="item">
-                <i class="co fa fa-circle-thin" job="complete"></i>
-                <p class="text">${toDo}</p>
+                <i class="co fa ${DONE} complete" job="complete"></i>
+                <p class="text ${LINE}">${toDo}</p>
                 <i class="de fa fa-trash-o" job="delete"></i>
             </li>`
     const position = "beforeend";
